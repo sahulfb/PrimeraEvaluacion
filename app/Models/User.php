13881f44962cb50681386607a082a98761947972
Sahul Figueroa <sahulfb@gmail.com>
2023-08-21 10:52:26 -0800

    tarea tres

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laratrust\Contracts\LaratrustUser;
+use Laratrust\Traits\HasRolesAndPermissions;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasRolesAndPermissions, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
